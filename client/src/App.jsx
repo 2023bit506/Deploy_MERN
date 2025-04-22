45e0135b1a525a6083a9764e1e6ad5221aa71a91
Shubham Pradip Pawar <2023bit506@sggs.ac.in>
2025-04-22 23:59:48 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -6,10 +5,7 @@ import Users from './Users'
 import CreateUser from './CreateUser'
 import UpdateUser from './UpdateUser'
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
       <BrowserRouter>
@@ -23,5 +19,4 @@ function App() {
   )
 }
 
-export default App
-
+export default App;
