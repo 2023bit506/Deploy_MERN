65857c43771b751b046d3bf6200062470879ba22
Shubham Pradip Pawar <2023bit506@sggs.ac.in>
2025-04-22 22:51:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -58,8 +58,16 @@ const cors = require('cors');
 const UserModel = require('./models/Users');
 
 const app = express();
-app.use(cors());
+app.use(cors(
+    {
+        origin: ["https://deploy-mern-1whq.vercel.app"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 app.use(express.json());
+
+
 
 // Connect to MongoDB
 mongoose.connect("mongodb://127.0.0.1:27017/crud", {
