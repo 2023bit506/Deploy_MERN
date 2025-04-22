04cbb4e6eb79bee165c1449787b109aa8bbd64c0
Shubham Pradip Pawar <2023bit506@sggs.ac.in>
2025-04-22 23:30:47 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -60,7 +60,7 @@ const UserModel = require('./models/Users');
 const app = express();
 app.use(cors(
     {
-        origin: ["https://deploy-mern-1whq.vercel.app"],
+        origin: ["https://deploy-mern-frontend-beryl.vercel.app/"],
         methods: ["POST", "GET"],
         credentials: true
     }
